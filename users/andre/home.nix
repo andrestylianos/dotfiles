42f62fb4c74407d77c4a73bce28e3e3c4cca15e2
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-02-17 05:10:02 +0000

    basic nix dotfiles setup + home-manager

---
+++ users/andre/home.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "andre";
+  home.homeDirectory = "/home/andre";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "qt";
+  };
+
+  home.packages = with pkgs; [
+    git
+    git-crypt
+    gnupg
+    pinentry-qt
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
