80754ca6c038d142cf70e420c31723c6ac5c8f97
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-02-21 18:40:45 +0000

    increase emacs font size

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -28,7 +28,7 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 22 :weight 'bold))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 20 :weight 'bold))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
