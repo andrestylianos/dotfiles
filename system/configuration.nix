cb10e35673b7c7adfcf12b8deac4659f8cd88342
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-02-27 00:14:49 +0000

    working bluetooth in sway

--- system/configuration.nix
+++ system/configuration.nix
@@ -101,16 +101,16 @@ services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   # console.keyMap = "us";
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-    bluez_monitor.properties = {
-      ["bluez5.enable-sbc-xq"] = true,
-      ["bluez5.enable-msbc"] = true,
-      ["bluez5.enable-hw-volume"] = true,
-      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-                                                    }
-  '';
-  };
+  #environment.etc = {
+  #  "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #  bluez_monitor.properties = {
+  #    ["bluez5.enable-sbc-xq"] = true,
+  #    ["bluez5.enable-msbc"] = true,
+  #    ["bluez5.enable-hw-volume"] = true,
+  #    ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #                                                  }
+  #'';
+  #};
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.andre = {
@@ -193,6 +193,29 @@ hardware.opengl.extraPackages32 = with pkgs; [
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    media-session.config.bluez-monitor.rules = [
+    {
+      # Matches all cards
+      matches = [ { "device.name" = "~bluez_card.*"; } ];
+      actions = {
+        "update-props" = {
+          "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          # mSBC is not expected to work on all headset + adapter combinations.
+          "bluez5.msbc-support" = true;
+          # SBC-XQ is not expected to work on all headset + adapter combinations.
+          "bluez5.sbc-xq-support" = true;
+        };
+      };
+    }
+    {
+      matches = [
+        # Matches all sources
+        { "node.name" = "~bluez_input.*"; }
+        # Matches all outputs
+        { "node.name" = "~bluez_output.*"; }
+      ];
+    }
+  ];
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
   };
