c67738d328c6a4ebb8d759054865516365ce4f7d
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-08-04 22:47:50 +0100

    update rebuild bin script

--- bin/apply-system.sh
+++ bin/apply-system.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
-pushd ~/.dotfiles
-sudo nixos-rebuild switch --flake .#
-popd
+#!/bin/sh -e
+
+VERSION=1.0
+
+# Navigate to the directory of this script
+cd $(dirname $(readlink -f $0))
+cd ..
+
+build() {
+    if [ "$(uname)" == "Darwin" ]; then
+       darwin-rebuild switch --flake .# $@
+    elif [ "$(uname)" == "Linux" ]; then
+       sudo nixos-rebuild switch --flake .# $@
+    else
+       echo "Unknown platform"
+    fi
+}
+
+build
