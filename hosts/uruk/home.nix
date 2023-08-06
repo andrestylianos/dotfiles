88e5fce48b6bc0cdef43075ce670987694bf3a4b
André Stylianos Ramos <andre.stylianos@protonmail.com>
2023-08-06 18:12:32 +0100

    update uruk hostConfig

--- hosts/uruk/home.nix
+++ hosts/uruk/home.nix
@@ -9,10 +9,12 @@
     desktop = {
       hyprland.enable = true;
     };
+    direnv.enable = true;
     editor = {
       emacs.enable = true;
     };
     shell = {
+      starship.enable = true;
       zsh.enable = true;
     };
   };
